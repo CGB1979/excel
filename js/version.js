dd84b7e4a3c874adb6a4386fefb5b5df1f3ae344
CGB1979 <cristianbalduzzi@gmail.com>
2026-08-30 19:13:16 -0300

    Update version.js

--- js/version.js
+++ js/version.js
@@ -1,1 +1,1 @@
-window.APP_VERSION="1.0.0.a";document.addEventListener("DOMContentLoaded",()=>{const e=document.getElementById("versionBadge");if(e)e.textContent=`v${window.APP_VERSION}`});
+window.APP_VERSION="1.0.0.b";document.addEventListener("DOMContentLoaded",()=>{const e=document.getElementById("versionBadge");if(e)e.textContent=`v${window.APP_VERSION}`});
