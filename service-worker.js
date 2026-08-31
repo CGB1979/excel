a83dc0dffb37df339dcd1bfa4c9231e072a8e001
CGB1979 <cristianbalduzzi@gmail.com>
2026-08-31 20:46:12 -0300

    Update service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,28 @@
+const CACHE_NAME="convertidor-excel-v1.0.0.f";
+const ASSETS=[
+  "./",
+  "./index.html",
+  "./css/estilos.css",
+  "./js/version.js",
+  "./js/configuracionColumnas.js",
+  "./js/modal.js",
+  "./js/excel.js",
+  "./js/unificador.js",
+  "./js/codigosBarras.js",
+  "./js/app.js",
+  "./manifest.json"
+];
+self.addEventListener("install",event=>{
+  event.waitUntil(caches.open(CACHE_NAME).then(cache=>cache.addAll(ASSETS)).then(()=>self.skipWaiting()));
+});
+self.addEventListener("activate",event=>{
+  event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k)))).then(()=>self.clients.claim()));
+});
+self.addEventListener("fetch",event=>{
+  if(event.request.method!=="GET") return;
+  event.respondWith(fetch(event.request).then(response=>{
+    const copy=response.clone();
+    caches.open(CACHE_NAME).then(cache=>cache.put(event.request,copy));
+    return response;
+  }).catch(()=>caches.match(event.request)));
+});
